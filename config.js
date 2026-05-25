8fd3ff39c857b854f225782c4f2eb858c8737f0b
Liime-liime <2804916503@qq.com>
2026-05-25 21:04:43 +0800

    统一后端
    统一后端

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   // 本地调试: http://127.0.0.1:8000
   // Render 生产: https://cv-programme.onrender.com
-  API_BASE_URL: "http://127.0.0.1:8000"
+  API_BASE_URL: "https://cv-programme.onrender.com"
 };
